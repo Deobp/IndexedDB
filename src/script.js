b687c0ac8e2e8bfebcbf5e9dbe85261b38cd30c0
Sasha <zzsanchozz@gmail.com>
2020-08-29 04:55:12 +0300

    finished

--- src/script.js
+++ src/script.js
@@ -1,6 +1,44 @@
 let btn = document.getElementById('btn');
+let cd = document.getElementById('cdBtn');
+let view = document.getElementById('view');
+let db;
 
-btn.addEventListener('click', createDB)
+btn.addEventListener('click', createDB);
+
+cd.addEventListener('click', insertCd);
+
+view.addEventListener('click', viewCd);
+
+
+function viewCd() {
+    let tx = db.transaction('cdCollection', 'readonly');
+    let cdCollection = tx.objectStore('cdCollection');
+    let request = cdCollection.openCursor();
+
+    request.onsuccess = (e) => {
+        let cursor = e.target.result;
+        if (cursor) {
+            cursor.continue();
+        }
+    }
+    console.log(request)
+
+}
+
+
+function insertCd() {
+    let cd = {
+        title: 'winter songs',
+        artist: 'Grigoriy',
+    }
+    let tx = db.transaction('cdCollection', 'readwrite')
+
+    tx.onerror = (e) => alert(`error: ${e.target.error}`)
+    let cdCollection = tx.objectStore('cdCollection');
+    cdCollection.add(cd);
+}
+
+
 
 function createDB() {
     let database = document.getElementById('db');
@@ -9,16 +47,18 @@ function createDB() {
     const request = indexedDB.open(database.value, version.value);
 
     request.onupgradeneeded = (e) => {
-        alert('version upgraded')
+        db = e.target.result;
 
+
+        let cdCollection = db.createObjectStore('cdCollection', { keyPath: 'title' });
     }
 
     request.onsuccess = (e) => {
-        console.log(e)
-        alert('success is called')
+        db = e.target.result;
+
     }
 
     request.onerror = (e) => {
-        alert('error')
+        alert(`following error ${e.target.error}`)
     }
 }
