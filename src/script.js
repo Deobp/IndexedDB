0b445aee631dc901a22067615600f8f0d6bf8627
Sasha <zzsanchozz@gmail.com>
2020-08-30 02:53:19 +0300

    add new branch v4

--- src/script.js
+++ src/script.js
@@ -1,64 +1,29 @@
-let btn = document.getElementById('btn');
-let cd = document.getElementById('cdBtn');
-let view = document.getElementById('view');
+let submit = document.getElementById('submit');
+let input = document.getElementById('input');
 let db;
-
-btn.addEventListener('click', createDB);
-
-cd.addEventListener('click', insertCd);
-
-view.addEventListener('click', viewCd);
+let request = indexedDB.open('firstDB', 1);
 
 
-function viewCd() {
-    let tx = db.transaction('cdCollection', 'readonly');
-    let cdCollection = tx.objectStore('cdCollection');
-    let request = cdCollection.openCursor();
 
-    request.onsuccess = (e) => {
-        let cursor = e.target.result;
-        if (cursor) {
-            cursor.continue();
-        }
-    }
-    console.log(request)
+
+request.onupgradeneeded = (e) => {
+    db = e.target.result;
+    let cdCollection = db.createObjectStore('cdCollection', { keyPath: 'title' });
+}
+
+request.onsuccess = (e) => {
+    db = e.target.result;
 
 }
 
-
-function insertCd() {
-    let cd = {
-        title: 'winter songs',
-        artist: 'Grigoriy',
-    }
-    let tx = db.transaction('cdCollection', 'readwrite')
-
-    tx.onerror = (e) => alert(`error: ${e.target.error}`)
-    let cdCollection = tx.objectStore('cdCollection');
-    cdCollection.add(cd);
+request.onerror = (e) => {
+    alert(`following error ${e.target.error}`)
 }
 
+function wirteDB() {
+    let txWrite = db.transaction('cdCollection', 'readwrite');
+    let a = txWrite.objectStore('cdCollection');
+    txWrite.onerror = (e) => alert(`error: ${e.target.error}`)
 
-
-function createDB() {
-    let database = document.getElementById('db');
-    let version = document.getElementById('version');
-
-    const request = indexedDB.open(database.value, version.value);
-
-    request.onupgradeneeded = (e) => {
-        db = e.target.result;
-
-
-        let cdCollection = db.createObjectStore('cdCollection', { keyPath: 'title' });
-    }
-
-    request.onsuccess = (e) => {
-        db = e.target.result;
-
-    }
-
-    request.onerror = (e) => {
-        alert(`following error ${e.target.error}`)
-    }
+    a.add({ 'title': 'value' }); //write here avlue to add in db
 }
