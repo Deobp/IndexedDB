5c0b0d18f1defb5d72004cb769866935fa01da9e
Sasha <zzsanchozz@gmail.com>
2020-08-30 07:30:23 +0300

    add data to indexedDB, read data from indexedDB

--- src/script.js
+++ src/script.js
@@ -1,14 +1,15 @@
-let submit = document.getElementById('submit');
+let submit = document.getElementById('form');
 let input = document.getElementById('input');
+let ul = document.getElementById('list');
 let db;
 let request = indexedDB.open('firstDB', 1);
+let arr = [];
 
-
-
+form.addEventListener("submit", writeIndexedDB);
 
 request.onupgradeneeded = (e) => {
     db = e.target.result;
-    let cdCollection = db.createObjectStore('cdCollection', { keyPath: 'title' });
+    let cdCollection = db.createObjectStore('claster', { keyPath: 'value' });
 }
 
 request.onsuccess = (e) => {
@@ -20,10 +21,36 @@ request.onerror = (e) => {
     alert(`following error ${e.target.error}`)
 }
 
-function wirteDB() {
-    let txWrite = db.transaction('cdCollection', 'readwrite');
-    let a = txWrite.objectStore('cdCollection');
-    txWrite.onerror = (e) => alert(`error: ${e.target.error}`)
+function writeIndexedDB(event) {
+    let txWrite = db.transaction('claster', 'readwrite');
+    let access = txWrite.objectStore('claster');
+    txWrite.onerror = (e) => alert(`error: ${e.target.error}`);
+    access.add({ 'value': input.value });
+    input.value = '';
+    event.preventDefault(event);
+}
 
-    a.add({ 'title': 'value' }); //write here avlue to add in db
+function readIndexedDB() {
+    let txRead = db.transaction('claster', 'readonly');
+    let access = txRead.objectStore('claster');
+    let request = access.openCursor();
+
+    request.onsuccess = (e) => {
+        let cursor = e.target.result;
+        if (cursor) {
+            cursor.continue();
+        }
+        if (!arr.includes(cursor.key)) {
+            arr.push(cursor.key);
+            show(cursor.key);
+        }
+    }
+}
+
+arr.forEach(item => show(item));
+
+function show(item) {
+    let list = document.createElement("li");
+    list.textContent(item);
+    ul.appendChild(list);
 }
