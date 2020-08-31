8dc76fb03a0aef52e8270d2bb22494daca1fb001
Sasha <zzsanchozz@gmail.com>
2020-08-31 22:01:29 +0300

    updated

--- src/script.js
+++ src/script.js
@@ -1,56 +1,60 @@
-let submit = document.getElementById('form');
-let input = document.getElementById('input');
-let ul = document.getElementById('list');
+let submit = document.getElementById("form");
+let input = document.getElementById("input");
+let ul = document.getElementById("list");
 let db;
-let request = indexedDB.open('firstDB', 1);
+let request = indexedDB.open("firstDB", 1);
 let arr = [];
 
 form.addEventListener("submit", writeIndexedDB);
 
 request.onupgradeneeded = (e) => {
     db = e.target.result;
-    let cdCollection = db.createObjectStore('claster', { keyPath: 'value' });
-}
+
+    let claster = db.createObjectStore("claster", { keyPath: "value" });
+};
 
 request.onsuccess = (e) => {
     db = e.target.result;
-
-}
+    readIndexedDB();
+};
 
 request.onerror = (e) => {
-    alert(`following error ${e.target.error}`)
-}
+    alert(`following error ${e.target.error}`);
+};
 
 function writeIndexedDB(event) {
-    let txWrite = db.transaction('claster', 'readwrite');
-    let access = txWrite.objectStore('claster');
+    let txWrite = db.transaction("claster", "readwrite");
+    let access = txWrite.objectStore("claster");
     txWrite.onerror = (e) => alert(`error: ${e.target.error}`);
-    access.add({ 'value': input.value });
-    input.value = '';
+    let obj = {
+        value: input.value,
+        date: new Date().getDate(),
+    };
+
+    access.add(obj);
+    show(input.value);
+    input.value = "";
     event.preventDefault(event);
 }
 
 function readIndexedDB() {
-    let txRead = db.transaction('claster', 'readonly');
-    let access = txRead.objectStore('claster');
+    let txRead = db.transaction("claster", "readonly");
+    let access = txRead.objectStore("claster");
     let request = access.openCursor();
 
     request.onsuccess = (e) => {
         let cursor = e.target.result;
         if (cursor) {
-            cursor.continue();
-        }
-        if (!arr.includes(cursor.key)) {
             arr.push(cursor.key);
-            show(cursor.key);
+            cursor.continue();
+        } else {
+            arr.forEach((item) => show(item));
         }
-    }
+    };
 }
-
-arr.forEach(item => show(item));
 
 function show(item) {
     let list = document.createElement("li");
-    list.textContent(item);
+    list.textContent = item;
     ul.appendChild(list);
 }
